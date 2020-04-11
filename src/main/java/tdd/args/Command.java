dd845749d6f2afb44db87bbf2721243409d0204a
Seaborn Lee <seabornlee.cn@gmail.com>
2020-04-11 20:36:08 +0800

    refactoring: reduce cascade level

--- src/main/java/tdd/args/Command.java
+++ src/main/java/tdd/args/Command.java
@@ -1,35 +1,42 @@
 package tdd.args;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.ListIterator;
 import java.util.Map;
 
+import static java.util.Arrays.asList;
+
 public class Command {
-    Map<String, String> commands;
+    public static final String PARAM_NAME_PREFIX = "-";
+    private Map<String, String> keyValueMap;
 
     public Command(String commandLine) {
-        commands = new HashMap<>();
-        ListIterator<String> commandIterator = Arrays.asList(commandLine.split("\\s+")).listIterator();
+        keyValueMap = new HashMap<>();
+        parse(commandLine);
+    }
+
+    private void parse(String commandLine) {
+        ListIterator<String> commandIterator = asList(commandLine.split("\\s+")).listIterator();
         while (commandIterator.hasNext()) {
-            String name = commandIterator.next().substring(1);
-            if (commandIterator.hasNext()) {
-                String value = commandIterator.next();
-                if (isValue(value)) {
-                    commands.put(name, value);
-                } else {
-                    commandIterator.previous();
-                }
+            String name = commandIterator.next().substring(PARAM_NAME_PREFIX.length());
+            if (!commandIterator.hasNext()) {
+                break;
+            }
+            String nextString = commandIterator.next();
+            if (isValue(nextString)) {
+                keyValueMap.put(name, nextString);
+            } else {
+                commandIterator.previous();
             }
         }
     }
 
     public String getStringValue(String name) {
-        return commands.get(name);
+        return keyValueMap.get(name);
     }
 
     private boolean isValue(String value) {
-        if (value.charAt(0) == '-') {
+        if (value.charAt(0) == PARAM_NAME_PREFIX.charAt(0)) {
             if (value.length() > 2) {
                 return true;
             }
